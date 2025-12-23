df21119f51607a8439808e9bf6931793c6be2af5
Romeo Valentin <romeov@stanford.edu>
2025-12-22 23:58:12 -0800

    Add runtests.jl file with Project.toml

---
+++ .ci/runtests.jl
@@ -0,0 +1,17 @@
+using Pluto, Test
+
+notebookfiles = [
+    joinpath((isinteractive() ? pwd() : @__FILE__),
+             "..", "project$(i)", "project$(i).jl") for i in 1:3
+]
+
+@testset for notebookfile in notebookfiles
+    @show notebookfile
+    session = Pluto.ServerSession()
+    notebook = Pluto.SessionActions.open(session, notebookfile; run_async=false);
+    @test all(c -> c.errored == false, values(notebook.cells))
+    @test Pluto.WorkspaceManager.eval_fetch_in_workspace((session, notebook),
+        :(all([pass_small, pass_medium, pass_large]))
+    )
+end
+
