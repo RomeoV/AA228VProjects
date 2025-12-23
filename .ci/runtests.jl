91a83af6161a2de04ad2dbef8a4e1f6cac55d994
Romeo Valentin <romeov@stanford.edu>
2025-12-23 00:55:39 -0800

    ci: Readd test, add some debug logging

--- .ci/runtests.jl
+++ .ci/runtests.jl
@@ -9,9 +9,24 @@ notebookfiles = [
     @show notebookfile
     session = Pluto.ServerSession()
     notebook = Pluto.SessionActions.open(session, notebookfile; run_async=false);
-    #@test all(c -> c.errored == false, values(notebook.cells))
-    #@test Pluto.WorkspaceManager.eval_fetch_in_workspace((session, notebook), :(pass_small))
-    #@test Pluto.WorkspaceManager.eval_fetch_in_workspace((session, notebook), :(pass_medium))
-    #@test Pluto.WorkspaceManager.eval_fetch_in_workspace((session, notebook), :(pass_large))
+
+    println("___ NOTEBOOK LOGS START ___")
+    for (cell_id, cell) in notebook.cells
+        if !isempty(cell.logs)
+            println("Logs for cell $(cell_id):")
+            for log in cell.logs
+                # Pluto logs are dict-like. "msg" is the main text.
+                # Depending on Pluto version, it might be a Dict or a Struct.
+                msg = get(log, "msg", log) 
+                println("  [$(get(log, "level", "INFO"))] $msg")
+            end
+        end
+    end
+    println("___ NOTEBOOK LOGS END ___")
+
+    @test all(c -> c.errored == false, values(notebook.cells))
+    @test Pluto.WorkspaceManager.eval_fetch_in_workspace((session, notebook), :(pass_small))
+    @test Pluto.WorkspaceManager.eval_fetch_in_workspace((session, notebook), :(pass_medium))
+    @test Pluto.WorkspaceManager.eval_fetch_in_workspace((session, notebook), :(pass_large))
 end
 
